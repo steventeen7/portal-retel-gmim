ad6a8b65a56b956842730f666ea8f01eb5c0ba12
steventeen <henceruindungan@gmail.com>
2026-04-21 01:06:05 +0800

    fix: import localDB in supabase.ts to resolve build error

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -1,5 +1,5 @@
 import { supabase } from '../supabase/client';
-import { User, SoalTes, NilaiUser, SoalWawancara, Materi } from './local';
+import { User, SoalTes, NilaiUser, SoalWawancara, Materi, localDB } from './local';
 
 function isUUID(uuid: string) {
   return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid);
